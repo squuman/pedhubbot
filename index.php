47974a33f6fe39b893725915beb56c3cbbf2a4de
squuman <56595343+squuman@users.noreply.github.com>
2020-12-02 22:16:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require __DIR__ .'/vendor/autoload.php';
 $ch = curl_init("http://squuman.beget.tech/pedhubbot/index.php");
 # Setup request to send json via POST.
 $payload = json_encode($_REQUEST);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $_REQUEST );
 curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 # Return response instead of printing.
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
