d1cd89dd12e93a84aad6c912820e1831068c504d
squuman <56595343+squuman@users.noreply.github.com>
2020-12-02 21:55:54 +0300

    Update index.php
    curl

--- index.php
+++ index.php
@@ -5,7 +5,15 @@ use Longman\TelegramBot\Request;
 
 require __DIR__ .'/vendor/autoload.php';
 
-$result = Request::sendMessage([
-    'chat_id' => $_REQUEST['chat_id'],
-    'text'    => 'sosat',
-]);
+$ch = curl_init("http://squuman.beget.tech/pedhubbot/index.php");
+# Setup request to send json via POST.
+$payload = json_encode($_REQUEST);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+# Return response instead of printing.
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+# Send request.
+$result = curl_exec($ch);
+curl_close($ch);
+# Print response.
+echo "<pre>$result</pre>";
